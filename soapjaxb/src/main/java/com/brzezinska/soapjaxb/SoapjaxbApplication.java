66b3ddca2d007df5c044950af1a2d5087ac3fbbf
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-04-16 03:02:51 +0200

    improvements of consuming soapui

---
+++ soapjaxb/src/main/java/com/brzezinska/soapjaxb/SoapjaxbApplication.java
@@ -0,0 +1,13 @@
+package com.brzezinska.soapjaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapjaxbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapjaxbApplication.class, args);
+    }
+
+}
---
+++ soapjaxb/src/test/java/com/brzezinska/soapjaxb/SoapjaxbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brzezinska.soapjaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapjaxbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
